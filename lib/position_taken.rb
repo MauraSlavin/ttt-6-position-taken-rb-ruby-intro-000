4ed20618963be2dbab9def9fa66a83704419526a
Maura Slavin <mauramslavin@gmail.com>
2018-09-05 00:40:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index) do
-   board[index-1] !== " "
+   if board[index-1] !== " "
+     x = true
+   else
+     x = false
+   end # if
 end  # of def
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
