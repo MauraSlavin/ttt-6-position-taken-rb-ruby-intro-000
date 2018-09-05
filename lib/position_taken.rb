6e3fe2543f9459bdca61c214d9ec8ccc457b6951
Maura Slavin <mauramslavin@gmail.com>
2018-09-05 01:13:57 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,9 +8,11 @@ def position_taken?(board, index)
      puts "Make x false"
      x = false
      puts "X: #{x}"
+     x = false
    else
      puts "Make x true"
      x = true
      puts "X: #{x}"
+     x = true
   end # if
 end  # of def
