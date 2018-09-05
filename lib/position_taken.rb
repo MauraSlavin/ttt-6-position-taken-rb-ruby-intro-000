146ee349f93cdbc726f81d5e1390ddec42b27936
Maura Slavin <mauramslavin@gmail.com>
2018-09-05 01:10:32 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,16 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  puts board
-  puts index
+  puts "Board: #{board}"
+  puts "Index: #{index}"
   cell = board[index-1]
-  puts cell
+  puts "Cell: #{cell}"
    if (cell == " " || cell == nil)
      puts "Make x false"
      x = false
+     puts "X: #{x}"
    else
      puts "Make x true"
      x = true
+     puts "X: #{x}"
   end # if
 end  # of def
